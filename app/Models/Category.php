599f41f49f99a5d85f494a842ad157dc31bda905
Gafar Zade <qafarzade2014@gmail.com>
2021-03-23 14:32:24 +0400

    Category bug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $table = 'categories';
-    protected $fillable = ['name', 'slug'];
+    protected string $table = 'categories';
+    protected array $fillable = ['name', 'slug'];
 
-    public function products()
+    /**
+     * @return HasMany
+     */
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class,'category_id');
     }
